8cb441d44bc104dc8423be84012ddb54402776fc
Midwire <chris@midwiretech.com>
2011-01-06 01:08:12 -0600

    added some tests and implemented a few more api calls

---
+++ lib/delicious-api-via-oauth/delicious/post.rb
@@ -0,0 +1,42 @@
+module Delicious
+
+  class Post
+    attr_accessor :url
+    attr_accessor :description
+    attr_accessor :extended
+    attr_accessor :tags
+    attr_accessor :date
+    attr_accessor :meta
+    attr_accessor :hash
+    attr_accessor :others
+
+    def initialize(attrs)
+      @url = attrs[:url]
+      @description = attrs[:description]
+      @extended = attrs[:extended]
+      @tags = attrs[:tags]
+      @date = attrs[:date]
+      @meta = attrs[:meta]
+      @hash = attrs[:hash]
+      @others = attrs[:others]
+    end
+
+    def self.import_from_xml(xml)
+      doc = Nokogiri::XML(xml)
+      posts = doc.css('post')
+      posts.collect do |post|
+        p = Post.new(
+          :url => post['href'],
+          :description => post['description'],
+          :extended => post['extended'],
+          :tags => post['tag'].split(' '),
+          :date => Time.parse(post['time']),
+          :meta => post['meta'],
+          :hash => post['hash'],
+          :others => post['others']
+        )
+      end
+    end
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'constants'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'delicious-api-via-oauth'
+
+class Test::Unit::TestCase
+end
